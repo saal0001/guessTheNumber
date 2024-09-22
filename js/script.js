6a1fe82fab5c689dab26aa216c8c0c41112a2393
saal0001 <saal0001@stud.kea.dk>
2024-09-22 19:25:25 +0200

    færdig

--- js/script.js
+++ js/script.js
@@ -20,47 +20,61 @@ const pbGuess = document.querySelector(".check")
 const again = document.querySelector(".again")
 
 let secretNumber = 5;
+
 function numberGenerator() {
     secretNumber = Math.trunc(Math.random() * 20 + 1)
 }
 
 function checkAnswer() {
-    let currentScore = Number(lblScore.textContent)
     let userInput = Number(inpGuess.value)
 
     if (userInput < secretNumber) {
         lblMessage.textContent = "number too low"
         document.body.style.backgroundColor = "#990F02"
-        currentScore--
-        lblScore.textContent = currentScore
 
     } else if (userInput > secretNumber) {
         lblMessage.textContent = "number too high"
         document.body.style.backgroundColor = "#990F02"
-        currentScore--
-        lblScore.textContent = currentScore
 
     } else {
         document.body.style.backgroundColor = "#255"
         lblMessage.textContent = "you win click try again if you wanna play another game"
         lblNumber.textContent = secretNumber;
-        if (currentScore > lblHighScore.textContent) {
+
+    }
+    return userInput
+}
+
+function score(){
+    let guessedAnswer = checkAnswer()
+    let currentScore = Number(lblScore.textContent)
+
+    if (guessedAnswer < secretNumber){
+        currentScore--
+        lblScore.textContent = currentScore
+    }
+    else if (guessedAnswer > secretNumber){
+        currentScore--
+        lblScore.textContent = currentScore
+    }
+    else {
+        if (currentScore>lblHighScore.textContent){
             lblHighScore.textContent = currentScore
         }
-
     }
 }
 
 
-    function resetGame() {
-        numberGenerator()
-        lblMessage.textContent = "Start guessing"
-        lblScore.textContent = 20;
-        lblNumber.textContent = "?"
-        inpGuess.value = ""
-        document.body.style.backgroundColor = "#222"
-    }
+function resetGame() {
+    numberGenerator()
+    lblMessage.textContent = "Start guessing"
+    lblScore.textContent = 20;
+    lblNumber.textContent = "?"
+    inpGuess.value = ""
+    document.body.style.backgroundColor = "#222"
+}
 
 
-    again.addEventListener("click", resetGame);
-    pbGuess.addEventListener("click", checkAnswer)
+again.addEventListener("click", resetGame);
+pbGuess.addEventListener("click", checkAnswer)
+pbGuess.addEventListener("click",score)
